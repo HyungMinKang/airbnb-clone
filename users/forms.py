258078fa869380e048956589b3e89749e5530d8f
Your Name <ramkid91@naver.com>
2020-03-11 08:44:36 +0900

    Airbnb Tenth
    consturcting- login , sign-up, verify email
    -----------------------------------------
    next: Log in with GitHub, Kakao

---
+++ users/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from . import models
+
+
+class LoginForm(forms.Form):
+
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
+
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+# ModelForm 일일이 프로그래머가 필드를 설계할 필요없이 모델에서 갖다쓸 수 있게 해줌
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email", "birthdate")
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    # username 을 email로 Override + password 암호화
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
+

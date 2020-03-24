65f29935560d53ee02ac96bcea5a1086fa1c97ef
Your Name <ramkid91@naver.com>
2020-03-24 16:30:24 +0900

    add host

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 DEBUG = True
 
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com"]
+ALLOWED_HOSTS = [".elasticbeanstalk.com", "airbnb-clone.com"]
 
 
 # Application definition
